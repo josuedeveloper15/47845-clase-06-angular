21c2db119f9d73cfb6484ecff072c3c4b58d4938
josuebaez61@gmail.com <josuebaez61@gmail.com>
2023-09-27 21:03:43 -0300

    ADD, reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ReactiveFormsComponent, TemplateDrivenComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Reactive Forms
+    ReactiveFormsModule,
+
+    // Template Driven
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
